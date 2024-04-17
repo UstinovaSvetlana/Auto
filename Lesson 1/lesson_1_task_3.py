6380da95d03bf716af2a08be8e8bdcc73049af1e
UstinovaSvetlana <ustinova.sunshine@mail.ru>
2024-04-17 20:00:10 +0400

    Добавлены задания

---
+++ Lesson 1/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+user_name = input('Sveta')
+
+print("Hello, " +  user_name)
---
+++ Lesson 1/lesson_1_task_4.py
@@ -0,0 +1,4 @@
+first_name = input('Sveta')
+last_name = input('Ustinova')
+
+print("Вас зовут: " + first_name + last_name)
