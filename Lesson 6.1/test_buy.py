129d2f20eae5905fffa3dcf97c71625aac3b1097
UstinovaSvetlana <ustinova.sunshine@mail.ru>
2024-05-20 07:40:00 +0400

    исправлены файлы с заданиями

--- Lesson 6.1/test_buy.py
+++ Lesson 6.1/test_buy.py
@@ -15,28 +15,28 @@ def test_byuing():
     driver.get("https://www.saucedemo.com/")
 
 # В поле username  введи значение 'standard_user'
-    username = driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').clear()
-    username = driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys("standard_user")
+    driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').clear()
+    driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys("standard_user")
 
 # В поле password введи значение 'secret_sauce'
-    password = driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').clear()
-    password = driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys("secret_sauce")
+    driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').clear()
+    driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys("secret_sauce")
 
     sleep(2)
 # Нажми кнопку Login
-    login_button = driver.find_element(By.ID,"login-button").click()
+    driver.find_element(By.ID,"login-button").click()
 
 
 # Добавь товары в корзину
-    add_button_backpack = driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
-    add_button_t_shirt = driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
-    add_button_onesie = driver.find_element(By.ID,"add-to-cart-sauce-labs-onesie").click()
+    driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.ID,"add-to-cart-sauce-labs-onesie").click()
 
 # Перейди в корзину
-    cart_button = driver.find_element(By.XPATH, "//html/body/div/div/div/div[1]/div[1]/div[3]/a").click()
+    driver.find_element(By.XPATH, "//html/body/div/div/div/div[1]/div[1]/div[3]/a").click()
 
 # Нажми checkout
-    checkout_button = driver.find_element(By.XPATH, "//*[@id='checkout']").click()
+    driver.find_element(By.XPATH, "//*[@id='checkout']").click()
 
 # Введи данные покупателя
     first_name = driver.find_element(By.ID, "first-name").clear()
--- Lesson 6.1/test_calculator.py
+++ Lesson 6.1/test_calculator.py
@@ -18,20 +18,20 @@ def test_calculator():
 
 
 # Очисти поле delay и введи значение '45'
-    delay_rield = driver.find_element(By.ID, 'delay').clear()
-    delay_rield = driver.find_element(By.ID, 'delay').send_keys("45")
+    driver.find_element(By.ID, 'delay').clear()
+    driver.find_element(By.ID, 'delay').send_keys("45")
 
 
 # Нажми 7 + 8 =
-    seven_button = driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[1]').click()
-    plus_button = driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[4]').click()
-    eigth_button = driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[2]').click()
-    result_button = driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[15]').click()
+    driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[1]').click()
+    driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[4]').click()
+    driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[2]').click()
+    driver.find_element(By.XPATH, '//html/body/main/div/div[4]/div/div/div[2]/span[15]').click()
 
 # Покажи результат
     wait = WebDriverWait(driver, 45)
     wait.until(EC. text_to_be_present_in_element((By.CSS_SELECTOR, '[class = "screen"]'), '15'))
-    result = driver.find_element(By.CSS_SELECTOR, '[class = "screen"]').text()
+    result = driver.find_element(By.CSS_SELECTOR, '[class = "screen"]').text
 
 # sum_nums = result_element.text
     print(result)
