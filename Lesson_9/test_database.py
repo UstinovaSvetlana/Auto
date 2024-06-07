a7dd65b162c2632be3ed2ac526a310e3e271b7f1
UstinovaSvetlana <ustinova.sunshine@mail.ru>
2024-06-07 07:42:14 +0400

    дополнен файл test_database

--- Lesson_9/test_database.py
+++ Lesson_9/test_database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from faker import Faker
 
 db_connection_string = "postgresql://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx"
+
+fake = Faker("ru_RU")
 
 def test_db_connection():
     db = create_engine(db_connection_string)
